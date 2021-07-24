43450b5ecf1e13d26e295704d5e0948a4929cbe3
Bruno Martins <bruno.eletr@gmail.com>
2021-07-24 11:56:05 -0300

    Primeiro commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,26 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Livros livro = new Livros();
+		
+		
+		System.out.println(gato);
+		System.out.println(livro);
+		// TODO Auto-generated method stub
+		/*int a = 2;
+		int b = 2;
+		System.out.println("Hello World! " + (a + b));*/
+	}
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+	
+}
